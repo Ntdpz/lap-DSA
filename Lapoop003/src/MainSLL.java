48d52e205c93eb0243e7b7ea979bf952d6bbc871
NZ_NTDPZZ <97873968+Ntdpz@users.noreply.github.com>
2022-02-04 00:04:06 +0700

    Add files via upload

---
+++ Lapoop003/src/MainSLL.java
@@ -0,0 +1,13 @@
+public class MainSLL {
+	public static void main(String[] args) {		
+	    SLL sll = new SLL();
+	    sll.addFirst(2);
+	    sll.addLast(3);
+	    sll.addFirst(1);
+	    sll.print();
+	    sll.removeFirst();
+	    sll.print();
+	    sll.removeLast();
+	    sll.print();
+	}
+}
